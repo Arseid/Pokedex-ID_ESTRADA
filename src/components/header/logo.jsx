287e08170b6d62bf035f111b391d0b68f0fbc1a7
Arseid <yvesrobert.estrada@gmail.com>
2023-02-28 12:43:12 +0100

    Logo to "/"

--- src/components/header/logo.jsx
+++ src/components/header/logo.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import logoPokedex from "../../ressources/logoPokedex.svg";
+import { Link } from "react-router-dom";
 
 const Logo = ({ size = 7 }) => {
-    return <img src={logoPokedex} alt="logo-pokedex" height={size * 10} />;
+    return <div>
+        <Link to="/"><img src={logoPokedex} alt="logo-pokedex" height={size * 10} /></Link>
+    </div>;
 };
 
 export default Logo;

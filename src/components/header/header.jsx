09c5582999ea2d3679b7fa1b400807e51cd5ed52
YVES-ROBERT ESTRADA <yves-robert.estrada@probtp.com>
2023-04-06 15:18:45 +0200

    removed css from header.jsx

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,17 +1,32 @@
 import React from "react";
 import Logo from "../logo/logo";
 import LanguageSelection from "../languageSelection/languageSelection";
-import "./header.css";
+import styled from "styled-components";
+
+const HeaderContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const LogoContainer = styled.div``;
+
+const LanguageSelectionContainer = styled.div`
+  margin-right: 20px;
+`;
 
 const Header = () => {
-    return <div className="Header-Container">
-        <div className="Header-Logo">
-            <Logo/>
-        </div>
-        <div className="Header-LanguageSelection">
-            <LanguageSelection/>
-        </div>
-    </div>
+    return (
+        <HeaderContainer>
+            <LogoContainer>
+                <Logo />
+            </LogoContainer>
+            <LanguageSelectionContainer>
+                <LanguageSelection />
+            </LanguageSelectionContainer>
+        </HeaderContainer>
+    );
 };
 
 export default Header;

8b84cfb4a6b43553c2fe23067fcbc76f883cd0a1
YVES-ROBERT ESTRADA <yves-robert.estrada@probtp.com>
2023-04-06 12:57:16 +0200

    mui + languageselection into mui almost finished

--- src/components/languageSelection/languageSelection.jsx
+++ src/components/languageSelection/languageSelection.jsx
@@ -1,26 +1,62 @@
-import React, {useContext} from "react";
-import {BiWorld} from 'react-icons/bi';
-import "./languageSelection.css";
-import {LanguageContext} from "../../context/context";
+import React, { useContext } from "react";
+import { BiWorld } from "react-icons/bi";
+import { LanguageContext } from "../../context/context";
+import Select from "@mui/material/Select";
+import MenuItem from "@mui/material/MenuItem";
+import Box from "@mui/material/Box";
 
 const LanguageSelection = () => {
-    const {setSelectedLanguage} = useContext(LanguageContext)
+    const { setSelectedLanguage } = useContext(LanguageContext);
 
-    return <div className="LanguageSelect-Container">
-        <BiWorld color={"white"} className="LanguageSelect-WorldIcon" size={35}/>
-        <select onChange={(e) => setSelectedLanguage(e.target.value)} className="LanguageSelect-Select">
-            <option value="fr">Français</option>
-            <option value="en">English</option>
-            <option value="es">Español</option>
-            <option value="it">Italiano</option>
-            <option value="ja">Japanese</option>
-            <option value="roomaji">Roomaji</option>
-            <option value="ko">Korean</option>
-            <option value="zh-Hans">Simplified Chinese</option>
-            <option value="zh-Hant">Traditional Chinese</option>
-            <option value="ja-Hrkt">Kana Japanese</option>
-        </select>
-    </div>;
+    return (
+        <Box display="flex" alignItems="center">
+            <BiWorld
+                color={"white"}
+                className="LanguageSelect-WorldIcon"
+                size={35}
+                sx={{ mr: 1 }}
+            />
+            <Select
+                onChange={(e) => setSelectedLanguage(e.target.value)}
+                className="LanguageSelect-Select"
+                defaultValue=""
+                sx={{
+                    "& .MuiSelect-select": {
+                        fontSize: "2rem",
+                        textAlign: "center",
+                        display: "flex",
+                        alignItems: "center",
+                        justifyContent: "center",
+                        color: "white"
+                    },
+                    "& .MuiOutlinedInput-root": {
+                        borderColor: "white",
+                        "&:hover": {
+                            borderColor: "white"
+                        },
+                        "& .MuiOutlinedInput-notchedOutline": {
+                            borderColor: "white"
+                        },
+                        "&.Mui-focused .MuiOutlinedInput-notchedOutline": {
+                            borderColor: "white"
+                        }
+                    }
+                }}
+            >
+                <MenuItem value="fr">Français</MenuItem>
+                <MenuItem value="en">English</MenuItem>
+                <MenuItem value="es">Español</MenuItem>
+                <MenuItem value="it">Italiano</MenuItem>
+                <MenuItem value="ja">Japanese</MenuItem>
+                <MenuItem value="roomaji">Roomaji</MenuItem>
+                <MenuItem value="ko">Korean</MenuItem>
+                <MenuItem value="zh-Hans">Simplified Chinese</MenuItem>
+                <MenuItem value="zh-Hant">Traditional Chinese</MenuItem>
+                <MenuItem value="ja-Hrkt">Kana Japanese</MenuItem>
+            </Select>
+        </Box>
+    );
 };
+
 
 export default LanguageSelection;

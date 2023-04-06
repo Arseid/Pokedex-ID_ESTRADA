668ebbf0c4eacb9709e664304e026fbf42f98cc2
YVES-ROBERT ESTRADA <yves-robert.estrada@probtp.com>
2023-04-06 15:09:55 +0200

    styled compononent installed + removed css from pokemonListPage.jsx

--- src/components/pokemonListPage/pokemonListPage.jsx
+++ src/components/pokemonListPage/pokemonListPage.jsx
@@ -1,8 +1,37 @@
 import React from "react";
 import SearchBar from "../searchBar/searchBar";
 import PokemonList from "../pokemonList/pokemonList";
-import './pokemonListPage.css';
+import styled from "styled-components";
 import {PokemonContext} from "../../context/context";
+
+const PokemonListPageContainer = styled.div`
+  width: 100%;
+`;
+
+const LoaderContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  margin-top: 2rem;
+`;
+
+const Loader = styled.div`
+  display: inline-block;
+  width: 5rem;
+  height: 5rem;
+  border: 3px solid white;
+  border-top-color: #292c33;
+  border-radius: 50%;
+  animation: spin 1s ease-in-out infinite;
+
+  @keyframes spin {
+    to {
+      transform: rotate(360deg);
+    }
+  }
+`;
+
 
 const PokemonListPage = () => {
 
@@ -13,16 +42,18 @@ const PokemonListPage = () => {
         setSearchInputValue(e.target.value);
     }
 
-    return <div className='PokemonListPage'>
-        <SearchBar searchInputValue={searchInputValue} searchPokemon={searchPokemon}/>
-        {!(Object.keys(pokemonTypes).length>0&&pokemonList.length>0) ?
-            <div className="PokemonListPage-LoaderContainer">
-                <div></div>
-            </div>
-            :
-            <PokemonList searchInputValue={searchInputValue}/>
-        }
-    </div>;
+    return (
+        <PokemonListPageContainer>
+            <SearchBar searchInputValue={searchInputValue} searchPokemon={searchPokemon} />
+            {!(Object.keys(pokemonTypes).length > 0 && pokemonList.length > 0) ? (
+                <LoaderContainer>
+                    <Loader/>
+                </LoaderContainer>
+            ) : (
+                <PokemonList searchInputValue={searchInputValue} />
+            )}
+        </PokemonListPageContainer>
+    );
 };
 
 export default PokemonListPage;

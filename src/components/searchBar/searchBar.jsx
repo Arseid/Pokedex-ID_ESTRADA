bce72090bd0bf52ff5231358c26869a258fff328
YVES-ROBERT ESTRADA <yves-robert.estrada@probtp.com>
2023-04-06 14:45:11 +0200

    search bar into mui

--- src/components/searchBar/searchBar.jsx
+++ src/components/searchBar/searchBar.jsx
@@ -1,13 +1,27 @@
 import React from "react";
-import './searchBar.css';
+import TextField from "@mui/material/TextField";
 
-const SearchBar = ({searchInputValue, searchPokemon}) => {
-
-
-
-    return <div className='SearchBar'>
-        <input placeholder='Enter a pokemon name' value={searchInputValue} onChange={searchPokemon} autoFocus/>
-    </div>;
+const SearchBar = ({ searchInputValue, searchPokemon }) => {
+    return (
+        <div style={{display: 'flex', justifyContent: 'center'}}>
+            <TextField
+                placeholder="Enter a pokemon name"
+                value={searchInputValue}
+                onChange={searchPokemon}
+                autoFocus
+                fullWidth
+                variant="outlined"
+                sx={{
+                    "& .MuiOutlinedInput-input": {
+                        fontSize: "1.7rem",
+                        height: "3rem",
+                        color: 'white'
+                    },
+                }}
+            />
+        </div>
+    );
 };
+
 
 export default SearchBar;
